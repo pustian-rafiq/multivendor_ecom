2ba5d07c110383934fcc2c82196e363890da6b5c
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-06-08 16:15:03 +0600

    admin template setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix'=>'admin','middleware'=>'auth'],function(){
+
+    Route::get('/',[AdminController::class,'Dashboard'])->name('admin');
+});
